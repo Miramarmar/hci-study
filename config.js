3e0782733779dfb3fc91e31b8d230254f133cb7f
Miramarmar <amira.mouhammed@gmail.com>
2026-02-18 13:32:00 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
 const SUPABASE_URL   = 'https://xvaokacldkgofkoldpdu.supabase.co';
 const SUPABASE_ANON  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2YW9rYWNsZGtnb2Zrb2xkcGR1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MTMyNTc2NCwiZXhwIjoyMDg2OTAxNzY0fQ.OqaGtCYUbSO_WqBWpGjjLT46BsakX9r-AhSFpPpnSLk';
-const GEMINI_API_KEY = 'AIzaSyBTVahljaKw20ZogNk0awcIVlB2vN0C9As';
+const GEMINI_API_KEY = 'AIzaSyAqs9Zk9s-IQKd8YvsxLC1cxYv7CKdBq4w';
 };
