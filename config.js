9f61953efaa190723f8cfbbc3cc38cbc105887e9
Miramarmar <amira.mouhammed@gmail.com>
2026-02-18 14:40:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
 const SUPABASE_URL   = 'https://xvaokacldkgofkoldpdu.supabase.co';
-const SUPABASE_ANON  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2YW9rYWNsZGtnb2Zrb2xkcGR1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MTMyNTc2NCwiZXhwIjoyMDg2OTAxNzY0fQ.OqaGtCYUbSO_WqBWpGjjLT46BsakX9r-AhSFpPpnSLk';
+const SUPABASE_ANON  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2YW9rYWNsZGtnb2Zrb2xkcGR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMjU3NjQsImV4cCI6MjA4NjkwMTc2NH0.dfGr0I7-nmRPq0t2ZV2a9xJ2yi5h6nhDffT-0nsM_-8';
 const GEMINI_API_KEY = 'AIzaSyAqs9Zk9s-IQKd8YvsxLC1cxYv7CKdBq4w';
 };
