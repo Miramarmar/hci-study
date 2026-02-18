c3b406da049062c4c1ff71cfb8f0bcbda8d146c1
Miramarmar <amira.mouhammed@gmail.com>
2026-02-18 14:45:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,3 +3,4 @@ const SUPABASE_URL   = 'https://xvaokacldkgofkoldpdu.supabase.co';
 const SUPABASE_ANON  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2YW9rYWNsZGtnb2Zrb2xkcGR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMjU3NjQsImV4cCI6MjA4NjkwMTc2NH0.dfGr0I7-nmRPq0t2ZV2a9xJ2yi5h6nhDffT-0nsM_-8';
 const GEMINI_API_KEY = 'AIzaSyAqs9Zk9s-IQKd8YvsxLC1cxYv7CKdBq4w';
 };
+
